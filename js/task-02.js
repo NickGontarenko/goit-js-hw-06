4b633067b6873279973d8598722ae4f882efe6ae
NickGontarenko <nick.gontarenko@gmail.com>
2022-01-18 23:09:11 +0200

    Update task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listOfIngredientsRef = document.querySelector('#ingredients');
+
+const CreatesSeparateElement = elements => {
+  return elements.map(element => {
+    const listRef = document.createElement('li');
+    listRef.textContent = element;
+    listRef.classList.add('item');
+    return listRef;
+  });
+};
+const separateElements = CreatesSeparateElement(ingredients);
+listOfIngredientsRef.append(...separateElements);
