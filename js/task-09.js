e06c7f3023b81b297ca3d209e80589312d6d6767
NickGontarenko <nick.gontarenko@gmail.com>
2022-01-22 18:49:19 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorValueRef = document.querySelector('.color');
+const changeColorBtnRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+
+changeColorBtnRef.addEventListener('click', onChangeColorBody);
+
+function onChangeColorBody() {
+    colorValueRef.textContent = getRandomHexColor();
+    bodyRef.style.backgroundColor = getRandomHexColor();
+};
