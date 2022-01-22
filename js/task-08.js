ad93a807e35f8b4d60bc5a186be49a52d758a51f
NickGontarenko <nick.gontarenko@gmail.com>
2022-01-22 18:13:54 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onLoginFormSubmit);
+
+function onLoginFormSubmit(event) {
+    event.preventDefault();
+
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    const message = 'Предупреждение !!!! Все поля должны быть заполнены';
+
+    if (email === '' || password === '') {
+        alert(message);
+    }
+
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+
+    formRef.reset();
+}
