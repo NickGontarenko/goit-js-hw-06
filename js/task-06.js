eedccf485befe033e046c6bd46ec956edd4832f8
NickGontarenko <nick.gontarenko@gmail.com>
2022-01-25 23:30:36 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,16 @@
 const validationInputRef = document.querySelector('#validation-input');
+const dataValue = validationInputRef.dataset.length;
 
 validationInputRef.addEventListener('blur', onCharacterСountСheck);
 
 function onCharacterСountСheck(event) {
-    const currentNumberOfCharacters = event.currentTarget.value.length;
+    const currentNumber = event.currentTarget.value.length;
 
-    if (currentNumberOfCharacters >= validationInputRef.dataset.length) {
-        validationInputRef.classList.remove('invalid');
+    validationInputRef.classList.remove('invalid', 'valid');
+
+    if (currentNumber == dataValue) {
         validationInputRef.classList.add('valid');
     } else {
-        validationInputRef.classList.remove('valid');
         validationInputRef.classList.add('invalid');
     }
 }
